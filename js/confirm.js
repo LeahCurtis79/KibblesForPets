814a91264bc36422001b15d8787c461ef9c03df7
LeahCurtis79 <80548573+LeahCurtis79@users.noreply.github.com>
2021-06-28 19:12:12 -0500

    Update confirm.js

--- js/confirm.js
+++ js/confirm.js
@@ -8,9 +8,9 @@ window.onload = function() {
 	document.getElementById('email').value = getCookie('email');
 	document.getElementById('phone').value = getCookie('phone');
 	
-	document.getElementById('interestMusic').checked = getCookie('interestMusic');
-	document.getElementById('interestSports').checked = getCookie('interestSports');
-	document.getElementById('interestComputers').checked = getCookie('interestComputers');
+	document.getElementById('interestPitbulls').checked = getCookie('interestPitbulls');
+	document.getElementById('interestLabs').checked = getCookie('interestLabs');
+	document.getElementById('interestMastiffs').checked = getCookie('interestMastiffs');
 	document.getElementById('signUpYes').checked = getCookie('signUpNewsletterYes');
 	document.getElementById('signUpNo').checked = getCookie('signUpNewsletterNo');
 	document.getElementById('comments').value = getCookie('comments');
